b79d5d39c30c1b7d6f424d767b8ca0b9c7bedb66
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-16 15:54:22 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 2, "male", "blue", "easy", 0);
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 12, "female", "purple", "rambunctious", 1);
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 20, "male", "brownish", "can't complain", 0);
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 1, "female", "blue", "optimistic", 1);
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 100, "male", "green", "miserly", 0);
+INSERT INTO bears 
+  (name, age, gender, color, temperament, alive)
+  VALUES (, 2, "male", "blue", "easy", 0);
