165be652ff329cef825194f9e07697deffb8cce0
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-16 17:37:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name;"
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldes
   "Write your SQL query here"
 end
 
